ab4343dbe18c6a731979739531c0246e79d141b9
tworiver0931 <63793194+tworiver0931@users.noreply.github.com>
2023-05-16 00:58:42 +0900

    added RF

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
+import itertools
 from tqdm import tqdm
 
 from utils import FFDataset, SoftmaxDataset
@@ -62,7 +63,7 @@ class FFLayer(nn.Module):
         h_neg = self.forward(h_neg)
 
         return h_pos.detach(), h_neg.detach(), loss_history
-    
+
     def predict(self, x):
         x = self.normalize(x)
         return F.relu(self.linear(x))
@@ -76,6 +77,193 @@ class FFNN(nn.Module):
         for d in range(len(dims) - 1):
             self.fflayers.append(
                 FFLayer(dims[d], dims[d+1], threshold, lr, batch_size, epochs, dropout, device))
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+
+        pos_outputs = []
+        total_loss_history = []
+
+        for i, layer in enumerate(self.fflayers):
+            print(f'Training {i} ff-layer')
+            h_pos, h_neg, loss_history = layer.train(h_pos, h_neg)
+
+            pos_outputs.append(h_pos)
+            total_loss_history.append(loss_history)
+
+        return pos_outputs, total_loss_history
+
+    def predict(self, x):
+        outputs = []
+        for i, layer in enumerate(self.fflayers):
+            x = layer.predict(x)
+            outputs.append(x)
+        return outputs
+
+
+class LocalConv(nn.Module):
+    def __init__(self,
+                 out_channels,
+                 input_shape,
+                 kernel_size,
+                 strides,
+                 bias=False,
+                 ):
+        super().__init__()
+
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.bias = bias
+
+        input_row, input_col = input_shape[1:]
+        input_filter = input_shape[0]
+
+        output_row = self.output_length(
+            input_row, self.kernel_size[0], self.strides[0])
+        output_col = self.output_length(
+            input_col, self.kernel_size[1], self.strides[1])
+
+        self.output_shape = (output_row, output_col)
+
+        self.kernel_shape = (output_row * output_col,
+                             self.kernel_size[0] *
+                             self.kernel_size[1] * input_filter,
+                             self.out_channels
+                             )
+
+        self.kernel = nn.Parameter(torch.randn(self.kernel_shape))
+
+        if self.bias:
+            self.bias_shape = (self.out_channels, output_row, output_col)
+            self.bias = nn.Parameter(torch.randn(self.bias_shape))
+
+    def output_length(self, input_length, kernel_size, stride):
+        output_length = input_length - kernel_size + 1
+        return (output_length + stride - 1) // stride
+
+    def forward(self, inputs):
+        feature_dim = self.kernel_shape[1]
+        channels_out = self.kernel_shape[-1]
+
+        xs = []
+        output_axes_ticks = [range(axis_max) for axis_max in self.output_shape]
+        for position in itertools.product(*output_axes_ticks):
+            slices = [slice(None)]
+            slices.append(slice(None))
+            slices.extend(
+                slice(
+                    position[d] * self.strides[d],
+                    position[d] * self.strides[d] + self.kernel_size[d],
+                )
+                for d in [0, 1]
+            )
+            xs.append(torch.reshape(inputs[slices], (1, -1, feature_dim)))
+
+        x_aggregate = torch.cat(xs)
+
+        output = torch.bmm(x_aggregate, self.kernel)
+        output = torch.reshape(
+            output, (self.output_shape + (-1, channels_out)))
+        output = torch.permute(output, (2, 3, 0, 1))
+
+        if self.bias is not False:
+            output = output + self.bias.unsqueeze(0)
+        return output
+
+
+class FFRFLayer(nn.Module):
+    def __init__(self,
+                 out_channel,
+                 input_shape,
+                 kernel_size,
+                 strides,
+                 threshold,
+                 lr,
+                 batch_size,
+                 epochs,
+                 dropout,
+                 device
+                 ):
+
+        super().__init__()
+
+        self.local_conv = LocalConv(
+            out_channel, input_shape, kernel_size, strides, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+        self.peer_norm = nn.BatchNorm2d(out_channel)
+
+        self.opt = torch.optim.Adam(self.parameters(), lr=lr)
+        self.threshold = threshold
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.device = device
+
+        self.criterion = nn.BCEWithLogitsLoss()
+
+    def forward(self, x):
+        x = x / (x.norm(p=2, dim=(1, 2, 3), keepdim=True) + 1e-7)
+        return F.relu(self.dropout(self.peer_norm(self.local_conv(x))))
+
+    def train(self, h_pos, h_neg):
+        dataset = FFDataset(h_pos, h_neg)
+        dataloader = DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=True)
+
+        bar = tqdm(range(self.epochs))
+        loss_history = []
+
+        for _ in bar:
+            epoch_loss = 0
+            for h_pos_batch, h_neg_batch in dataloader:
+                g_pos = self.forward(h_pos_batch).pow(2).mean(dim=(1, 2, 3))
+                g_neg = self.forward(h_neg_batch).pow(2).mean(dim=(1, 2, 3))
+
+                loss = self.criterion(torch.cat([g_pos-self.threshold, g_neg-self.threshold]),
+                                      torch.cat([torch.ones((g_pos.shape[0]), device=self.device),
+                                                 torch.zeros((g_neg.shape[0]), device=self.device)]))
+
+                self.opt.zero_grad()
+                loss.backward()
+                self.opt.step()
+
+                epoch_loss += loss.item()
+
+            epoch_loss /= len(dataloader)
+            bar.set_postfix({'Loss': epoch_loss})
+            loss_history.append(epoch_loss)
+
+        h_pos = self.forward(h_pos)
+        h_neg = self.forward(h_neg)
+
+        return h_pos.detach(), h_neg.detach(), loss_history
+
+    def predict(self, x):
+        x = x / (x.norm(p=2, dim=(1, 2, 3), keepdim=True) + 1e-7)
+        return F.relu(self.peer_norm(self.local_conv(x)))
+
+
+class FFRFNN(nn.Module):
+    def __init__(self,
+                 out_channel_list,
+                 input_shape_list,
+                 kernel_size_list,
+                 strides_list,
+                 threshold,
+                 lr,
+                 batch_size,
+                 epochs,
+                 dropout,
+                 device
+                 ):
+
+        super().__init__()
+
+        self.fflayers = nn.ModuleList()
+        for d in range(len(out_channel_list)):
+            self.fflayers.append(
+                FFRFLayer(out_channel_list[d], input_shape_list[d], kernel_size_list[d], strides_list[d], threshold, lr, batch_size, epochs, dropout, device))
 
     def train(self, x_pos, x_neg):
         h_pos, h_neg = x_pos, x_neg
--- trainer.py
+++ trainer.py
@@ -1,14 +1,25 @@
 import torch
-from model import FFNN, SoftmaxLayer
+from model import FFNN, FFRFNN, SoftmaxLayer
 from utils import create_neg
 
 
-class Trainer:
-    def __init__(self, train_set, ff_dims, out_dim, threshold=1, lr=0.001, batch_size=512, epochs=100, dropout=0, device='cuda:0'):
+class FCTrainer:
+    def __init__(self,
+                 train_set,
+                 ff_dims,
+                 out_dim,
+                 threshold=1,
+                 lr=0.001,
+                 batch_size=512,
+                 epochs=100,
+                 dropout=0,
+                 device='cuda:0'
+                 ):
+
         self.device = device
 
         self.x_pos, self.y = train_set.data, train_set.targets
-        
+
         # normalize data
         self.x_pos = self.x_pos.type(torch.float32) / 255
         self.mean, self.std = self.x_pos.mean(), self.x_pos.std()
@@ -23,8 +34,10 @@ class Trainer:
         self.x_neg = self.x_neg.to(device)
         self.y = self.y.to(device)
 
-        self.ffnn = FFNN(ff_dims, threshold, lr, batch_size, epochs, dropout, device)
-        self.softmaxlayer = SoftmaxLayer(sum(ff_dims[2:]), out_dim, lr, batch_size, epochs)
+        self.ffnn = FFNN(ff_dims, threshold, lr, batch_size,
+                         epochs, dropout, device)
+        self.softmaxlayer = SoftmaxLayer(
+            sum(ff_dims[2:]), out_dim, lr, batch_size, epochs)
 
         self.ffnn.to(device)
         self.softmaxlayer.to(device)
@@ -51,9 +64,104 @@ class Trainer:
         x_test = x_test.reshape(x_test.shape[0], -1)
         x_test = x_test.to(self.device)
         y_test = y_test.to(self.device)
-        
+
         with torch.no_grad():
             test_outputs = self.ffnn.predict(x_test)
+            test_cat = torch.cat(test_outputs[1:], dim=1)
+            preds = self.softmaxlayer(test_cat)
+
+        acc = (torch.argmax(preds, 1) == y_test).float().mean()
+        print('test error: ', 1 - acc.item())
+
+
+class RFTrainer:
+    def __init__(self,
+                 train_set,
+                 out_channel_list,
+                 input_shape_list,
+                 kernel_size_list,
+                 strides_list,
+                 out_dim,
+                 threshold=10,
+                 lr=0.001,
+                 batch_size=512,
+                 epochs=100,
+                 dropout=0,
+                 device='cuda:0'
+                 ):
+
+        self.device = device
+
+        self.x_pos, self.y = train_set.data, train_set.targets
+
+        # normalize data
+        self.x_pos = self.x_pos.type(torch.float32) / 255
+        self.mean, self.std = self.x_pos.mean(), self.x_pos.std()
+        self.x_pos = (self.x_pos - self.mean) / self.std
+
+        self.x_neg = create_neg(self.x_pos)
+
+        self.x_pos = self.x_pos.unsqueeze(1)
+        self.x_neg = self.x_neg.unsqueeze(1)
+
+        self.x_pos = self.x_pos.to(device)
+        self.x_neg = self.x_neg.to(device)
+        self.y = self.y.to(device)
+
+        self.ffnn = FFRFNN(out_channel_list,
+                           input_shape_list,
+                           kernel_size_list,
+                           strides_list,
+                           threshold,
+                           lr,
+                           batch_size,
+                           epochs,
+                           dropout,
+                           device)
+
+        softmax_input_dim = 0
+        for shape in input_shape_list[1:]:
+            dim = 1
+            for n in shape:
+                dim *= n
+            softmax_input_dim += dim
+
+        self.softmaxlayer = SoftmaxLayer(
+            softmax_input_dim, out_dim, lr, batch_size, epochs)
+
+        self.ffnn.to(device)
+        self.softmaxlayer.to(device)
+
+        self.ffnn_his = None
+        self.softmax_his = None
+
+    def train(self):
+        print('\n[Start training FF-Layers]')
+        pos_outputs, self.ffnn_his = self.ffnn.train(self.x_pos, self.x_neg)
+
+        # use positive data outputs except first ff-layer
+        pos_outputs = [output.reshape(output.shape[0], -1)
+                       for output in pos_outputs]
+        pos_cat = torch.cat(pos_outputs[1:], dim=1)
+
+        print('\n[Start training Softmax-Layer]')
+        self.softmax_his = self.softmaxlayer.train(pos_cat, self.y)
+
+    def test(self, test_set):
+        print('[Test]')
+
+        x_test, y_test = test_set.data, test_set.targets
+        x_test = x_test.type(torch.float32) / 255
+        x_test = (x_test - self.mean) / self.std
+
+        x_test = x_test.unsqueeze(1)
+        x_test = x_test.to(self.device)
+        y_test = y_test.to(self.device)
+
+        with torch.no_grad():
+            test_outputs = self.ffnn.predict(x_test)
+            test_outputs = [output.reshape(output.shape[0], -1)
+                            for output in test_outputs]
             test_cat = torch.cat(test_outputs[1:], dim=1)
             preds = self.softmaxlayer(test_cat)
 
